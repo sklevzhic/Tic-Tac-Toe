ae3bdb6db055dfafefd8a62eb042c96fba9defda
Alexander <sklevzhic@gmail.com>
2022-07-17 15:48:24 +0300

    added new branch

--- src/types/values.ts
+++
@@ -1,9 +0,0 @@
-export enum Values {
-  VALUE_X= "X",
-  VALUE_0 = "0"
-}
-
-export enum Operators {
-  INC= "+",
-  DEC = "-"
-}
--- src/utils/getCurrentValue.ts
+++
@@ -1,5 +0,0 @@
-import {Values} from "../types/values";
-
-export function getCurrentValue(step: number): Values {
-  return step % 2 ? Values.VALUE_0 : Values.VALUE_X
-}
